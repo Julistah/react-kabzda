6969a5f9611606d3ab86e2c0a4a2036ee7b696af
Julistah <46353346+Julistah@users.noreply.github.com>
2021-11-21 14:40:31 +0200

    22

--- src/App.js
+++ src/App.js
@@ -9,18 +9,16 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/profile' element={<Profile />}/>
-                        <Route path='/dialogs' element={<Dialogs />}/>
-                    </Routes>
-                </div>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path='/profile' element={<Profile/>}/>
+                    <Route path='/dialogs/*' element={<Dialogs/>}/>
+                </Routes>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
-    return <div>
-        Dialogs
-    </div>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="dialogs/1">
+                        valera
+                    </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="dialogs/2">
+                        nikita
+                    </NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className="message"> a</div>
+                <div className="message"> f</div>
+                <div className="message"> c</div>
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs;
