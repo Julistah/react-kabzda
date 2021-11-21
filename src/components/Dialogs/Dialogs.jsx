4ec883a075477654d595ba24c40387c8f306c7e3
Julistah <46353346+Julistah@users.noreply.github.com>
2021-11-21 15:32:21 +0200

    23

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,25 +2,38 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path = props.id;
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>
+                {props.name}
+            </NavLink>
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>
+            {props.message}
+        </div>
+    )
+}
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to="dialogs/1">
-                        valera
-                    </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="dialogs/2">
-                        nikita
-                    </NavLink>
-                </div>
+                <DialogItem name="Dimych" id="1"/>
+                <DialogItem name="Alla" id="2"/>
+                <DialogItem name="Vova" id="3"/>
+                <DialogItem name="Anyone" id="4"/>
             </div>
             <div className={s.messages}>
-                <div className="message"> a</div>
-                <div className="message"> f</div>
-                <div className="message"> c</div>
+                <Message message="Hello"/>
+                <Message message="How are you"/>
+                <Message message="Hello world)))"/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,23 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
-    return <div>
-        my posts
-        <div>
-            <textarea></textarea>
-            <button>Add post</button>
+    return (
+        <div className={s.postsBlock}>
+            my posts
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
+            </div>
+            <div className={s.posts}>
+                <Post message='Hi, how are you' likesCount='20'/>
+                <Post message='Hi, First post' likesCount='10'/>
+            </div>
         </div>
-        <Post message='Hi, how are you' likesCount='20'/>
-        <Post message='Hi, First post' likesCount='10'/>
-    </div>
+    )
+
 }
 export default MyPosts;
