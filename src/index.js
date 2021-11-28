587504b7118e22c3691e1c17517b7cb280ff556c
Julistah <46353346+Julistah@users.noreply.github.com>
2021-11-28 11:13:51 +0200

    35

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {subscribe} from "./redux/state";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {addPost, updateNewPostText} from "./redux/state";
+
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App posts={state.profilePage.posts}
+                     messages={state.dialogsPage.messages}
+                     dialogs={state.dialogsPage.dialogs}
+                     friends={state.friendsPage.friends}
+                     newPostText={state.profilePage.newPostText}
+                     addPost={addPost}
+                     updateNewPostText={updateNewPostText}/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
 
 rerenderEntireTree(state);
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('state changed');
+}
 
 let state = {
 
@@ -33,7 +35,6 @@ let state = {
                 image: 'https://socialvk.ru/wp-content/uploads/avatarka-pustaya-vk_0.jpg'
             }
         ],
-
         messages: [ //sernder: 1 -other guy, 0 -you
             {id: 1, message: 'Hello', sender: 1},
             {id: 2, message: 'How are you', sender: 1},
@@ -63,7 +64,7 @@ let state = {
 
 }
 
-export let addPost = () =>{
+export const addPost = () =>{
     let newPost ={
         id: 5,
         post: state.profilePage.newPostText,
@@ -74,9 +75,13 @@ export let addPost = () =>{
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) =>{
+export const updateNewPostText = (newText) =>{
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
