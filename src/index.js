fecf38977cf2e7e08ca2675642476e65cdf80247
Julistah <46353346+Julistah@users.noreply.github.com>
2022-01-03 19:32:21 +0200

    47

--- src/index.js
+++ src/index.js
@@ -7,29 +7,22 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App
-                    friends={store.getState().friendsPage.friends}
-                    // dispatch={store.dispatch.bind(store)}
-                    // store={store}
-                />
-            </Provider>
 
-            {/*bind -чтоб в MyPosts.jsx в addPost props.addPost(); чтоб эдд пост имел нормальный this (37)*/}
-        </BrowserRouter>,
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App
+                friends={store.getState().friendsPage.friends}
+                // dispatch={store.dispatch.bind(store)}
+                // store={store}
+            />
+        </Provider>
+
+        {/*bind -чтоб в MyPosts.jsx в addPost props.addPost(); чтоб эдд пост имел нормальный this (37)*/}
+    </BrowserRouter>,
     document.getElementById('root')
 )
-    ;
-}
 
 
-rerenderEntireTree(store.getState());
 
-store.subscribe(() => {
-        let state = store.getState();
-        rerenderEntireTree();
-    }
-);
+
