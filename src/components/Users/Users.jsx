b55281408932b719fe059caf24d5cbb63cb187ad
Julistah <46353346+Julistah@users.noreply.github.com>
2022-01-04 23:39:35 +0200

    51

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 import styles from './users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/anon.jpg'
 
 let Users = (props) => {
 
     if (props.users.length === 0){
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am a boss',
-                    location: {city: 'Kiev', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
-                    followed: false,
-                    fullName: 'Ivan',
-                    status: 'I am a boss',
-                    location: {city: 'Moskow', country: 'Ukraine'}
-                },
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        })
+
+        // props.setUsers([
+        //         {
+        //             id: 1,
+        //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
+        //             followed: false,
+        //             fullName: 'Dmitry',
+        //             status: 'I am a boss',
+        //             location: {city: 'Minsk', country: 'Belarus'}
+        //         },
+        //         {
+        //             id: 2,
+        //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
+        //             followed: true,
+        //             fullName: 'Sasha',
+        //             status: 'I am a boss',
+        //             location: {city: 'Kiev', country: 'Russia'}
+        //         },
+        //         {
+        //             id: 3,
+        //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
+        //             followed: false,
+        //             fullName: 'Ivan',
+        //             status: 'I am a boss',
+        //             location: {city: 'Moskow', country: 'Ukraine'}
+        //         },
+        //     ]
+        // )
     }
 
 
@@ -40,7 +46,7 @@ let Users = (props) => {
                 <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={styles.userPhoto}/>
+                            <img src={u.photos.small!=null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
@@ -51,7 +57,7 @@ let Users = (props) => {
                     <span>
                         <span>
                             <div>
-                                {u.fullName}
+                                {u.name}
                              </div>
                             <div>
                                 {u.status}
@@ -59,10 +65,10 @@ let Users = (props) => {
                         </span>
                         <span>
                             <div>
-                                {u.location.country}
+                                {"u.location.country"}
                             </div>
                             <div>
-                                {u.location.city}
+                                {"u.location.city"}
                             </div>
                         </span>
                     </span>
