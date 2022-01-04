a11333ca798b810661b0d6dd687322561505a659
Julistah <46353346+Julistah@users.noreply.github.com>
2022-01-04 23:47:36 +0200

    52 ska

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,42 +5,47 @@ import userPhoto from '../../assets/images/anon.jpg'
 
 let Users = (props) => {
 
-    if (props.users.length === 0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        })
+    let getUsers = () =>{
+        if (props.users.length === 0){
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
 
-        // props.setUsers([
-        //         {
-        //             id: 1,
-        //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
-        //             followed: false,
-        //             fullName: 'Dmitry',
-        //             status: 'I am a boss',
-        //             location: {city: 'Minsk', country: 'Belarus'}
-        //         },
-        //         {
-        //             id: 2,
-        //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
-        //             followed: true,
-        //             fullName: 'Sasha',
-        //             status: 'I am a boss',
-        //             location: {city: 'Kiev', country: 'Russia'}
-        //         },
-        //         {
-        //             id: 3,
-        //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
-        //             followed: false,
-        //             fullName: 'Ivan',
-        //             status: 'I am a boss',
-        //             location: {city: 'Moskow', country: 'Ukraine'}
-        //         },
-        //     ]
-        // )
+            // props.setUsers([
+            //         {
+            //             id: 1,
+            //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
+            //             followed: false,
+            //             fullName: 'Dmitry',
+            //             status: 'I am a boss',
+            //             location: {city: 'Minsk', country: 'Belarus'}
+            //         },
+            //         {
+            //             id: 2,
+            //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
+            //             followed: true,
+            //             fullName: 'Sasha',
+            //             status: 'I am a boss',
+            //             location: {city: 'Kiev', country: 'Russia'}
+            //         },
+            //         {
+            //             id: 3,
+            //             photoUrl: 'http://user-life.com/uploads/posts/2018-08/1535608847_kak-udalit-avatarku-ubrat-postavit-sdelat-zagruzit-dobavit-foto-vkontakte-dlya-telegramma-skaypa-vayber-diskorda.jpg',
+            //             followed: false,
+            //             fullName: 'Ivan',
+            //             status: 'I am a boss',
+            //             location: {city: 'Moskow', country: 'Ukraine'}
+            //         },
+            //     ]
+            // )
+        }
     }
 
 
+
+
     return <div>
+        <button onClick={getUsers}>GetUsers</button>
         {
             props.users.map(u =>
                 <div key={u.id}>
