d398e323619935152203c7ed11ef20fafdb90084
Julistah <46353346+Julistah@users.noreply.github.com>
2022-01-12 23:45:55 +0200

    some styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
+import logo from "../../assets/images/logonotfb.png"
 
 const Header = (props) => {
     return <header className={s.header}>
         <img
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png'
+            src={logo}
             alt='logo'
         />
 
         <div className={s.loginBlock}>
             { props.isAuth
-                ? <div>{props.login} - <button class="btn btn-dark" onClick={props.logout}>Log Out</button></div>
+                ? <div>{props.login} - <button className="btn btn-dark" onClick={props.logout}>Log Out</button></div>
                 : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
