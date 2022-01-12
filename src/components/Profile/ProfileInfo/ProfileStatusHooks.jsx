63857087d0c228a069b41b620822443a9d857572
Wander-man <wanderman.man.man@gmail.com>
2022-01-12 12:26:10 +0200

    85 - used hook useEffect() in profileStatusHooks to update local state if state from props changes to sync

--- src/components/Profile/ProfileInfo/ProfileStatusHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusHooks.jsx
@@ -1,24 +1,25 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 
 const ProfileStatusHooks = (props) => {
-
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
 
+    useEffect( () => {
+        setStatus(props.status)
+    }, [props.status]);
+
     const activateEditMode = () => {
         setEditMode(true);
-    }
-
+    };
     const deactivateEditMode = () => {
         setEditMode(false);
         props.updateStatus(status);
-    }
-
+    };
     const onStatusChange = (e) => {
         setStatus(e.currentTarget.value)
-    }
+    };
 
     return (
         <div>
