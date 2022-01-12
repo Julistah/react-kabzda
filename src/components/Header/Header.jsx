c4a903940216ba12fcdeac011c955cad0c9a5c15
Julistah <46353346+Julistah@users.noreply.github.com>
2022-01-12 23:06:30 +0200

    added Bootstrap <3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = (props) => {
 
         <div className={s.loginBlock}>
             { props.isAuth
-                ? <div>{props.login} - <button onClick={props.logout}>Log Out</button></div>
+                ? <div>{props.login} - <button class="btn btn-dark" onClick={props.logout}>Log Out</button></div>
                 : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MainApp from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 ReactDOM.render( <MainApp/>, document.getElementById('root'))
