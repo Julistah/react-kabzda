f5d171ec49b3ae1a3c0e07b134c710a560b9f312
Wander-man <wanderman.man.man@gmail.com>
2022-01-12 16:24:24 +0200

    91 - connected redux viewing extension

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import thunkMiddleware from "redux-thunk";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
@@ -16,10 +16,11 @@ let reducers = combineReducers({
     auth: authReducer,
     form: formReducer,
     app: appReducer
-})
+});
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
+window.__store__ = store;
 
 export default store;
